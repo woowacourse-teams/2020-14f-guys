0f255e19fecce6daca7ff6b7a591c5a0d77f827b
moon <mjj920930@gmail.com>
2020-07-13 19:49:26 +0900

    feat: 프론트엔드 화면 뼈대 구성
    - Navigation 구현

--- peloton-frontend/App.js
+++ peloton-frontend/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+
+import Root from "./component/Root";
 
 const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
+  <NavigationContainer>
+    <StatusBar />
+    <Root />
+  </NavigationContainer>
 );
-export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
---
+++ peloton-frontend/component/profile/Profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const Profile = () => {
+  return <View />;
+};
+
+export default Profile;
