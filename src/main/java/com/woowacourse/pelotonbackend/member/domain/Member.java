a1fb8f3d205b431a5919484fdcebd003faf88610
moon <mjj920930@gmail.com>
2020-07-15 15:01:13 +0900

    refactor: 불필요한 어노테이션 삭제
    - @AllArgsConstructor가 @Builder에 포함되어 있으므로 삭제

--- src/main/java/com/woowacourse/pelotonbackend/member/domain/Member.java
+++ src/main/java/com/woowacourse/pelotonbackend/member/domain/Member.java
@@ -15,13 +15,11 @@ import org.springframework.data.relational.core.mapping.Embedded;
 
 import com.woowacourse.pelotonbackend.vo.Cash;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.With;
 
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Builder
 @EqualsAndHashCode(of = "id")
 @Getter
