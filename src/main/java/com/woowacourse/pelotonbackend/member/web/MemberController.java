82d61410a60ee741926a5d5af5ff2747f9ec0610
moon <mjj920930@gmail.com>
2020-07-16 13:24:24 +0900

    refactor: Controller가 요청을 올바르게 받도록 수정
    - Controller의 Parameter에 @RequestBody, @Valid 추가
    - MemberRequest에 Validation 애너테이션 추가

--- src/main/java/com/woowacourse/pelotonbackend/member/web/MemberController.java
+++ src/main/java/com/woowacourse/pelotonbackend/member/web/MemberController.java
@@ -2,8 +2,11 @@ package com.woowacourse.pelotonbackend.member.web;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.woowacourse.pelotonbackend.member.domain.Member;
@@ -18,7 +21,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/api/members")
-    public ResponseEntity<Void> createMember(MemberRequest memberRequest) {
+    public ResponseEntity<Void> createMember(@RequestBody @Valid MemberRequest memberRequest) {
         Member member = memberService.createMember(memberRequest.toMember());
         return ResponseEntity
             .created(URI.create("/api/members/" + member.getId()))
--- src/main/java/com/woowacourse/pelotonbackend/member/web/dto/MemberRequest.java
+++ src/main/java/com/woowacourse/pelotonbackend/member/web/dto/MemberRequest.java
@@ -2,6 +2,10 @@ package com.woowacourse.pelotonbackend.member.web.dto;
 
 import java.beans.ConstructorProperties;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.woowacourse.pelotonbackend.member.domain.Member;
 import com.woowacourse.pelotonbackend.member.domain.Role;
 import com.woowacourse.pelotonbackend.vo.Cash;
@@ -14,9 +18,18 @@ import lombok.Getter;
 @Getter
 public class MemberRequest {
     private final Long id;
+
+    @NotBlank
     private final String name;
+
+    @NotBlank
+    @Email
     private final String email;
+
+    @NotNull
     private final Cash cash;
+
+    @NotNull
     private final Role role;
 
     public Member toMember() {
