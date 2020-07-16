c187a15c6820c6adaebf41f2547b6def7d457c77
BEOMJUN KIM <skyblue300a@gmail.com>
2020-07-16 11:21:57 +0900

    # This is a combination of 2 commits. # This is the 1st commit message:
    chore: webflux 의존성 추가
    
    # This is the commit message #2:
    
    refactor: 인수 테스트를 WebTestClient를 사용하여 테스트하도록 변경

--- src/test/java/com/woowacourse/pelotonbackend/report/ReportAcceptanceTest.java
+++ src/test/java/com/woowacourse/pelotonbackend/report/ReportAcceptanceTest.java
@@ -1,28 +1,17 @@
 package com.woowacourse.pelotonbackend.report;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.pelotonbackend.report.domain.ReportType;
 import com.woowacourse.pelotonbackend.report.infra.ReportCreateBody;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@AutoConfigureWebTestClient
 public class ReportAcceptanceTest {
     public static final Long MEMBER_ID = 1L;
 
@@ -33,18 +22,7 @@ public class ReportAcceptanceTest {
     public static final String DESCRIPTION = "설명";
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setup(WebApplicationContext webApplicationContext) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-            .addFilters(new CharacterEncodingFilter("UTF-8", true))
-            .alwaysDo(print())
-            .build();
-    }
+    private WebTestClient webTestClient;
 
     /**
      * Given 신고하는 유저의 ID, 신고할 인증의 ID가 주어진다.
@@ -52,14 +30,15 @@ public class ReportAcceptanceTest {
      * Then 신고가 완료된다.
      */
     @Test
-    void createReport() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(
-            post("/api/reports/certification/{certificationId}/member/{reportMemberId}", CERTIFICATION_ID, MEMBER_ID)
-                .content(objectMapper.writeValueAsBytes(new ReportCreateBody(REPORT_TYPE, DESCRIPTION)))
-                .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isCreated())
-            .andReturn();
-
-        assertThat(mvcResult.getResponse().getHeader("Location")).isNotNull();
+    void createReport() {
+        webTestClient.post()
+            .uri("/api/reports/certification/{certificationId}/member/{reportMemberId}", CERTIFICATION_ID, MEMBER_ID)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new ReportCreateBody(REPORT_TYPE, DESCRIPTION))
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectHeader()
+            .valueMatches("Location", "/api/reports/[0-9]+");
     }
 }
