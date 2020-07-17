61facdb9cf4f63ae38f45b8c33c8d84ac0541509
moon <mjj920930@gmail.com>
2020-07-17 16:34:07 +0900

    test: 인수테스트 Restassured로 바꾸기

--- src/test/java/com/woowacourse/pelotonbackend/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/com/woowacourse/pelotonbackend/member/acceptance/MemberAcceptanceTest.java
@@ -2,56 +2,65 @@ package com.woowacourse.pelotonbackend.member.acceptance;
 
 import static com.woowacourse.pelotonbackend.member.domain.MemberFixture.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.pelotonbackend.member.domain.Role;
 import com.woowacourse.pelotonbackend.member.web.dto.MemberRequest;
 import com.woowacourse.pelotonbackend.vo.Cash;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
 public class MemberAcceptanceTest {
+    // @formatter:off
 
     @Autowired
-    private WebTestClient client;
+    private ObjectMapper objectMapper;
+
+    @LocalServerPort
+    public int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    private static RequestSpecification given() {
+        return RestAssured.given().log().all();
+    }
+
+    /*
+    Scenario : 회원을 관리한다.
+        when : 회원을 만든다.
+        then : 회원이 생성된다.
+
+        when : 회원 정보를 읽어온다.
+        then : 회원 정보가 생성 정보와 일치한다.
+
+        when : 회원을 변경했을 때
+        then : 회원 정보를 읽어온다.
+        then : 회원 정보가 변경 정보와 일치한다.
+
+        when : 회원을 삭제한다.
+        then: 기존 회원이 삭제되었다.
+     */
 
     @DisplayName("회원을 관리하는 기능")
     @Test
-    void manageMember() {
-        //when
-        // 회원을 만든다.
+    void manageMember() throws JsonProcessingException {
         createMember(EMAIL, NAME, CASH, ROLE);
-
-        //then
-        // 회원이 생성된다.
-
-        //when
-        // 회원 정보를 읽어온다.
-
-        //then
-        // 회원 정보가 생성 정보와 일치한다.
-
-        //when
-        //회원을 변경했을 때
-
-        //then
-        // 회원 정보를 읽어온다.
-        // 회원 정보가 변경 정보와 일치한다.
-
-        //when
-        //회원을 삭제한다.
-
-        //then
-        // 기존 회원이 삭제되었다.
     }
 
-    private void createMember(final String email, final String name, final Cash cash, final Role role) {
+    private void createMember(String email, String name, Cash cash, Role role) throws JsonProcessingException{
         final MemberRequest memberRequest = MemberRequest.builder()
             .email(email)
             .name(name)
@@ -59,11 +68,18 @@ public class MemberAcceptanceTest {
             .role(role)
             .build();
 
-        client.post().uri("/api/members")
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(memberRequest)
-            .exchange()
-            .expectStatus().isCreated()
-            .expectHeader().valueEquals("Location", String.format("/api/members/%d", ID));
+        final String request = objectMapper.writeValueAsString(memberRequest);
+
+        given()
+            .body(request)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/api/members")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.CREATED.value())
+            .header("location", String.format("/api/members/%d", ID));
     }
+
+    // @formatter:on
 }
