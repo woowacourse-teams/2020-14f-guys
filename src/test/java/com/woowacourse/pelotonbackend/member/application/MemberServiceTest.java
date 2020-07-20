187321f6fd4ec42a2fbf4c4f7cab72c77c2b38a5
JinJuMoon <40536197+JinJuMoon@users.noreply.github.com>
2020-07-20 16:31:43 +0900

    회원을 생성하는 기능을 구현한다. (#91)
    * refactor: 불필요한 어노테이션 삭제
    
    - @AllArgsConstructor가 @Builder에 포함되어 있으므로 삭제
    
    * feat: Member 엔티티의 DTO 생성
    
    * feat: MemberController 및 테스트 구현
    
    - MemberController에 회원을 생성하는 메서드 구현
    - MemberControllerTest에 회원을 생성하는 테스트 구현
    
    * fix: @Nullable 사용 시 Warning 문구가 뜨는 문제 해결
    
    - Warning 문구 : "Warning:java: unknown enum constant javax.annotation.meta.When"
    
    * refactor: 테스트 패키지 이름을 프로덕션 패키지 이름과 일치하도록 수정, 사용하지 않는 import 삭제
    
    - controller/MemberControllerTest.java -> web/MemberControllerTest.java
    
    * feat: MemberService 및 테스트 구현
    
    - MemberService에 회원을 생성하는 메서드 구현
    - MemberServiceTest에 회원을 생성하는 테스트 구현
    
    * feat: Member 관련 테스트의 Fixture를 별도의 클래스로 생성
    
    - MemberControllerTest, MemberServiceTest, MemberRepositoryTest의 중복된 Test Fixtrue를 MemberFixture 클래스로 추출
    - assert문이 여러개인 경우 assertAll()로 묶음
    
    * feat: Member의 LocalDatTime 타입 필드에 final, wither 추가
    
    - createdAt, updatedAt 필드에 적용
    
    * refactor: 컨트롤러가 create 요청을 받을 경우 응답의 body가 없도록 수정
    
    - MemberController의 createMember 메서드의 반환 타입을 ResponseEntity<Void>로 수정
    - 회원 생성 요청에 대한 응답의 헤더에 Location 필드 값을 확인하도록 테스트 수정
    
    * refactor: Controller가 요청을 올바르게 받도록 수정
    
    - Controller의 Parameter에 @RequestBody, @Valid 추가
    - MemberRequest에 Validation 애너테이션 추가
    
    * test: Member 관련 인수테스트 작성
    
    * test: Member 관련 인수테스트의 클라이언트 객체를 WebClient로 변경
    
    * refactor: 지역변수에 final 추가
    
    * refactor: 결정된 팀 컨벤션으로 수정
    
    - 패키지 이름 변경하기(web, application, domain, infra)
    - Fixture 위치 도메인 패키지로 옮기기
    - 파라미터 final 붙이기
    
    * test: 인수테스트 Restassured로 바꾸기
    
    * refactor: Dto Service에서 변환하도록 구조 바꾸기
    
    * refactor: Dto Service에서 변환하도록 구조 바꾸기
    
    * refactor: 테스트에서 Member, MemberRefactor 등 반복적으로 사용되는 객체의 생성리 로직을 MemberFixtrue로 분
    
    * refactor: 패키지 이름 수정
    
    * refactor: 리뷰 반영
    
    - ObjectMapper의 writeValueAsString을 writeValueAsBytes로 변경하여 성능 향상
    - mockMvc의 andDo(print())메서드를 @BeforeEach에서 실행하도록 수정
    
    * refactor: 엔티티를 DTO로 바꾸어서 Controller로 반환하도록 수정
    
    * refactor: MemberRequest를 MemberCreateRequest로 변경
    
    * refactor: 테스트에서 객체의 필드를 한번에 비교하는 AssertJ 메서드 활용

---
+++ src/test/java/com/woowacourse/pelotonbackend/member/application/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package com.woowacourse.pelotonbackend.member.application;
+
+import static com.woowacourse.pelotonbackend.member.domain.MemberFixture.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.woowacourse.pelotonbackend.member.domain.Member;
+import com.woowacourse.pelotonbackend.member.domain.MemberFixture;
+import com.woowacourse.pelotonbackend.member.domain.MemberRepository;
+import com.woowacourse.pelotonbackend.member.presentation.dto.MemberCreateRequest;
+import com.woowacourse.pelotonbackend.member.presentation.dto.MemberResponse;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberService = new MemberService(memberRepository);
+    }
+
+    @DisplayName("회원을 생성한다")
+    @Test
+    void createMember() {
+        final MemberCreateRequest memberCreateRequest = MemberFixture.memberCreateRequest();
+        final Member persistMember = MemberFixture.memberWithId();
+
+        when(memberRepository.save(any(Member.class))).thenReturn(persistMember);
+
+        final MemberResponse memberResponse = memberService.createMember(memberCreateRequest);
+
+        assertAll(
+            () -> assertThat(memberResponse.getId()).isEqualTo(ID),
+            () -> assertThat(memberResponse.getEmail()).isEqualTo(EMAIL),
+            () -> assertThat(memberResponse.getName()).isEqualTo(NAME),
+            () -> assertThat(memberResponse.getCash()).isEqualTo(CASH),
+            () -> assertThat(memberResponse.getRole()).isEqualTo(ROLE)
+        );
+    }
+
+}
