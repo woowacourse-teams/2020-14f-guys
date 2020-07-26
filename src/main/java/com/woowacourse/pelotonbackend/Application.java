5e2ea8d4039ac6928125bdfb0f48afd3d06d22b2
dd <dqrd123@gmail.com>
2020-07-26 13:17:14 +0900

    refactor: profile에 따라 설정 파일을 사용하도록 수정

--- src/main/java/com/woowacourse/pelotonbackend/Application.java
+++ src/main/java/com/woowacourse/pelotonbackend/Application.java
@@ -1,19 +1,13 @@
 package com.woowacourse.pelotonbackend;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
-    private static final String PROPERTIES =
-        "spring.config.location="
-            + "classpath:/application.yml"
-            + ",classpath:/config/secret.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-            .properties(PROPERTIES)
-            .run(args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
