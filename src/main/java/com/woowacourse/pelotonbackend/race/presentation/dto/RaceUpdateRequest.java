a17d35feca5c2c459f6ec922401786f3d6cfeec9
dd <dqrd123@gmail.com>
2020-08-03 20:15:34 +0900

    refactor: jacoco test 커버리지에 맞게 수정
    - RaceUpdateRequest 모든 필드 수정하도록 수정
    - ErrorResponse 팩터리 메서드 삭제

--- src/main/java/com/woowacourse/pelotonbackend/race/presentation/dto/RaceUpdateRequest.java
+++ src/main/java/com/woowacourse/pelotonbackend/race/presentation/dto/RaceUpdateRequest.java
@@ -1,11 +1,8 @@
 package com.woowacourse.pelotonbackend.race.presentation.dto;
 
 import java.beans.ConstructorProperties;
-import java.util.Objects;
 
 import javax.validation.Valid;
-
-import org.springframework.lang.Nullable;
 
 import com.woowacourse.pelotonbackend.race.domain.DateDuration;
 import com.woowacourse.pelotonbackend.race.domain.Race;
@@ -23,54 +20,32 @@ import lombok.Getter;
 @Builder
 @Getter
 public class RaceUpdateRequest {
-    @Nullable
     private final String title;
 
-    @Nullable
     private final String description;
 
-    @Nullable
     @Valid
     private final DateDuration raceDuration;
 
-    @Nullable
     private final RaceCategory category;
 
-    @Nullable
     @Valid
     private final Cash entranceFee;
 
     // TODO: Url이 아닌 파일을 직접 업로드하도록 수정해야함
-    @Nullable
     private final ImageUrl certification;
 
-    @Nullable
     private final ImageUrl thumbnail;
 
     public Race toRace(final Race race) {
-        final Race.RaceBuilder builder = race.toBuilder();
-        if (Objects.nonNull(title)) {
-            builder.title(title);
-        }
-        if (Objects.nonNull(description)) {
-            builder.description(description);
-        }
-        if (Objects.nonNull(raceDuration)) {
-            builder.raceDuration(raceDuration);
-        }
-        if (Objects.nonNull(category)) {
-            builder.category(category);
-        }
-        if (Objects.nonNull(entranceFee)) {
-            builder.entranceFee(entranceFee);
-        }
-        if (Objects.nonNull(certification)) {
-            builder.certificationExample(certification);
-        }
-        if (Objects.nonNull(thumbnail)) {
-            builder.thumbnail(thumbnail);
-        }
-
-        return builder.build();
+        return race.toBuilder()
+            .title(this.title)
+            .description(this.description)
+            .raceDuration(this.raceDuration)
+            .category(this.category)
+            .entranceFee(this.entranceFee)
+            .certificationExample(this.certification)
+            .thumbnail(this.thumbnail)
+            .build();
     }
 }
