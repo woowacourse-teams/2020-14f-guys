ffca7e810eab8d4b5d42c43d39e82cf24e0432d7
dd <dqrd123@gmail.com>
2020-08-09 23:11:01 +0900

    refactor: TimeDuration JsonFormat 추가

--- src/main/java/com/woowacourse/pelotonbackend/mission/domain/DateTimeDuration.java
+++ src/main/java/com/woowacourse/pelotonbackend/mission/domain/DateTimeDuration.java
@@ -4,14 +4,18 @@ import java.time.LocalDateTime;
 
 import javax.validation.constraints.FutureOrPresent;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Value;
 
 @Value
 public class DateTimeDuration {
     @FutureOrPresent
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private final LocalDateTime startTime;
     // todo : 포맷팅 해결
+    // TODO: 2020/08/09 현재로써 어떻게 사용될 지 정확하지 않으므로 보류
 
     @FutureOrPresent
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private final LocalDateTime endTime;
 }
