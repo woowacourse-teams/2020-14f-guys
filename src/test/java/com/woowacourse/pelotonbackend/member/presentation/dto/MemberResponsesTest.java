ef07d46fec658c89c54527e97a83dfcb06dbb3ab
dd <dqrd123@gmail.com>
2020-08-10 14:14:33 +0900

    refactor: 변경값에 대한 상수화

--- src/test/java/com/woowacourse/pelotonbackend/member/presentation/dto/MemberResponsesTest.java
+++ src/test/java/com/woowacourse/pelotonbackend/member/presentation/dto/MemberResponsesTest.java
@@ -2,7 +2,6 @@ package com.woowacourse.pelotonbackend.member.presentation.dto;
 
 import static com.woowacourse.pelotonbackend.member.domain.MemberFixture.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,7 @@ class MemberResponsesTest {
     @DisplayName("MemberResponses Dto가 올바르게 Serialize되는지 확인한다.")
     @Test
     void memberResponsesTest() throws JsonProcessingException {
-        final String expectedValue = "{\"responses\":[{\"id\":1,\"kakao_id\":1,\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\"jinju\",\"email\":\"jj@woowa.com\",\"cash\":\"1\",\"role\":\"MEMBER\"},{\"id\":2,\"kakao_id\":2,\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\"sika\",\"email\":\"kyle@woowa.com\",\"cash\":\"1\",\"role\":\"MEMBER\"},{\"id\":3,\"kakao_id\":3,\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\"dd\",\"email\":\"dd@woowa.com\",\"cash\":\"1\",\"role\":\"MEMBER\"}]}";
+        final String expectedValue = "{\"responses\":[{\"id\":"+ID+",\"kakao_id\":"+KAKAO_ID+",\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\""+NAME+"\",\"email\":\""+EMAIL+"\",\"cash\":\"1\",\"role\":\"MEMBER\"},{\"id\":"+ID2+",\"kakao_id\":"+KAKAO_ID2+",\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\""+NAME2+"\",\"email\":\""+EMAIL2+"\",\"cash\":\"1\",\"role\":\"MEMBER\"},{\"id\":"+ID3+",\"kakao_id\":"+KAKAO_ID3+",\"profile\":\"https://lh3.googleusercontent.com/5EfQBHDb47tchiART6U6yk3yYS9qBYr6VUssB5wHE1AgavqV5E2SSuzyiNkc7UgVng\",\"name\":\""+NAME3+"\",\"email\":\""+EMAIL3+"\",\"cash\":\"1\",\"role\":\"MEMBER\"}]}";
 
         assertThat(objectMapper.writeValueAsString(memberResponses())).isEqualTo(expectedValue);
     }
