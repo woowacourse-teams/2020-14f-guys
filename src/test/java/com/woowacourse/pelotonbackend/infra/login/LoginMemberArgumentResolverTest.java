98dc4bbc54059584a19672c0bfaee37e50ac6133
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-08-11 16:36:48 +0900

    refactor: ArgumentResolver 예외 처리 및 테스트 구현 (#201)
    * refactor: ArgumentResolver에서 kakaoId가 유효하지 않을 경우에 대한 예외 처리 로직 추가 및 테스트 작성

--- src/test/java/com/woowacourse/pelotonbackend/infra/login/LoginMemberArgumentResolverTest.java
+++ src/test/java/com/woowacourse/pelotonbackend/infra/login/LoginMemberArgumentResolverTest.java
@@ -15,6 +15,7 @@ import org.springframework.core.MethodParameter;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.ServletWebRequest;
 
+import com.woowacourse.pelotonbackend.common.exception.MemberNotFoundException;
 import com.woowacourse.pelotonbackend.member.application.MemberService;
 import com.woowacourse.pelotonbackend.member.domain.LoginFixture;
 import com.woowacourse.pelotonbackend.member.domain.Member;
@@ -58,11 +59,36 @@ class LoginMemberArgumentResolverTest {
         assertThat(memberResponse).isEqualToComparingFieldByField(createWithId(ID));
     }
 
-    @DisplayName("kakaoId 정보가 없을 때 AssertionError로 예외처리한다.")
+    @DisplayName("해당 kakaoId와 매칭되는 회원정보가 존재하지 않을 경우 MemberNotFoundException으로 예외처리한다.")
+    @Test
+    void resolveArgumentTest2() {
+        final long kakaoId = 1L;
+        servletWebRequest.setAttribute("loginMemberKakaoId", String.valueOf(kakaoId), SCOPE_REQUEST);
+        given(memberService.findByKakaoId(kakaoId))
+            .willThrow(new MemberNotFoundException(String.format("Member(member kakaoId = %d) does not exist", kakaoId)));
+
+        assertThatThrownBy(
+            () -> loginMemberArgumentResolver.resolveArgument(methodParameter, null, servletWebRequest, null))
+            .isInstanceOf(MemberNotFoundException.class)
+            .hasMessageContaining("Member(member kakaoId = %d) does not exist", kakaoId);
+    }
+
+    @DisplayName("servletWebRequest attribute에 kakaoId 정보가 없을 때 AssertionError로 예외처리한다.")
     @Test
     void resolveArgumentTest3() {
-        assertThatThrownBy(() -> loginMemberArgumentResolver.resolveArgument(methodParameter, null,
-            servletWebRequest, null)).isInstanceOf(AssertionError.class)
-            .hasMessageContaining("Can not found 'loginMemberKakaoId");
+        assertThatThrownBy(
+            () -> loginMemberArgumentResolver.resolveArgument(methodParameter, null, servletWebRequest, null))
+            .isInstanceOf(AssertionError.class)
+            .hasMessageContaining("Cannot find loginMemberKakaoId");
+    }
+
+    @DisplayName("parseLong(kakaoId)에서 NumberFormatException이 발생할 경우 MemberNotFound로 예외처리한다.")
+    @Test
+    void resolveArgumentTest4() {
+        servletWebRequest.setAttribute("loginMemberKakaoId", "notLongId", SCOPE_REQUEST);
+        assertThatThrownBy(
+            () -> loginMemberArgumentResolver.resolveArgument(methodParameter, null, servletWebRequest, null))
+            .isInstanceOf(MemberNotFoundException.class)
+            .hasMessage("Cannot find Member");
     }
 }
---
+++ src/test/java/com/woowacourse/pelotonbackend/member/presentation/LoginMemberArgumentResolverIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.woowacourse.pelotonbackend.member.presentation;
+
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import com.woowacourse.pelotonbackend.common.ErrorResponse;
+import com.woowacourse.pelotonbackend.support.JwtTokenProvider;
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+
+/**
+ * ArgumentResolver에서 터진 예외가 GlobalExceptionHandler에서 잡히는지 확인하기 위한 통합 테스트
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LoginMemberArgumentResolverIntegrationTest {
+    private static final String NOT_EXIST_KAKAO_ID = "1000";
+
+    @LocalServerPort
+    private int port;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginMemberArgumentResolverIntegrationTest(
+        @Value("${secrets.jwt.token.secret-key}") final String secretKey,
+        @Value("${secrets.jwt.token.expire-length}") final long expireLength) {
+        this.jwtTokenProvider = new JwtTokenProvider(secretKey, expireLength);
+    }
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("존재하지 않는 kakaoId로 Member를 찾으려 할 때 MemberNotFoundExcetpion")
+    @Test
+    void notExistKakaoIdMemberRetrieve() {
+        final String token = jwtTokenProvider.createToken(NOT_EXIST_KAKAO_ID);
+        final ErrorResponse errorResponse = given()
+            .log().all()
+            .header(new Header("Authorization", "Bearer " + token))
+            .when()
+            .get("/api/members")
+            .then()
+            .statusCode(HttpStatus.NOT_FOUND.value())
+            .extract()
+            .body().as(ErrorResponse.class);
+
+        assertThat(errorResponse.getCode()).isEqualTo("Member-001");
+    }
+}
