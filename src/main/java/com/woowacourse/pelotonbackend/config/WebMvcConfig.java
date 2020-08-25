92f6ee93cf57cf8c0710761a9583d3aacf3abce1
KIMSIYOUNG <tldud2404@gmail.com>
2020-08-26 00:09:12 +0900

    feat: 개인정보처리방침 추가

--- src/main/java/com/woowacourse/pelotonbackend/config/WebMvcConfig.java
+++ src/main/java/com/woowacourse/pelotonbackend/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.woowacourse.pelotonbackend.member.presentation.LoginMemberArgumentResolver;
@@ -29,5 +30,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(argumentResolver);
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/secure").setViewName("secure.html");
     }
 }
